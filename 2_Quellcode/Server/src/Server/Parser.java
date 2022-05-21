d6191aed7d5676d9b80d5e0dcc47b18d6783dfa2
lukasrumpel <64522807+lukasrumpel@users.noreply.github.com>
2022-05-21 17:08:43 +0200

    Update Parser.java
    Aenderung Steuerzeichen Befehle, sowie TODOs

--- 2_Quellcode/Server/src/Server/Parser.java
+++ 2_Quellcode/Server/src/Server/Parser.java
@@ -6,7 +6,7 @@ public class Parser {
 	
 	public static String setStatus(String eventstr, boolean erledigt) {
 		String teilstr[];
-		teilstr = eventstr.split(",");
+		teilstr = eventstr.split("::");
 		
 		if(erledigt == true ) {
 			teilstr[3] = "erledigt";
@@ -15,14 +15,14 @@ public class Parser {
 			teilstr[3] = "unerledigt";
 		}
 		
-		eventstr = ","+ teilstr[1] + "," + teilstr[2]+  "," + teilstr[3] + ",";
+		eventstr = "::"+ teilstr[1] + "::" + teilstr[2]+  "::" + teilstr[3] + "::";
 		
 		return eventstr;
 	}
 	
 	public static String[] getAttr(String eventstr) {
 		String teilstr[];
-		teilstr = eventstr.split(",");
+		teilstr = eventstr.split("::");
 		return teilstr;
 	}
 	
@@ -39,7 +39,7 @@ public class Parser {
 	}
 	
 	public static boolean getStatus(String eventstr) {
-		String teilstr[] = eventstr.split(",");
+		String teilstr[] = eventstr.split("::");
 		if(teilstr[3].equals("true")) {
 			return true;
 		}
@@ -49,30 +49,30 @@ public class Parser {
 	}
 	
 	public static String getName(String eventstr) {
-		String teilstr[] = eventstr.split(",");
+		String teilstr[] = eventstr.split("::");
 		return teilstr[1];
 	}
 	
 	public static String setDate(String eventstr, int tag, int monat, int jahr) {
 		String teilstr[];
-		teilstr = eventstr.split(",");
+		teilstr = eventstr.split("::");
 		String datum = String.format("%02d", tag)+ "-" + String.format("%02d", monat) + "-" + String.format("%02d", jahr);
 		teilstr[2] = datum;
-		eventstr = ","+ teilstr[1] + "," + teilstr[2]+  "," + teilstr[3] + ",";
+		eventstr = "::"+ teilstr[1] + "::" + teilstr[2]+  "::" + teilstr[3] + "::";
 		return eventstr;
 	}
 	
 	public static String setName(String eventstr, String newname) {
 		String teilstr[];
-		teilstr = eventstr.split(",");
+		teilstr = eventstr.split("::");
 		teilstr[1] = newname;
-		eventstr = ","+ teilstr[1] + "," + teilstr[2]+  "," + teilstr[3] + ",";
+		eventstr = "::"+ teilstr[1] + "::" + teilstr[2]+  "::" + teilstr[3] + "::";
 		return eventstr;
 	}
 	
 	public static int parseCMD(String command) {
 		int cmd = 0;
-			String teilStr[] = command.split("/");
+			String teilStr[] = command.split("//");
 			if(teilStr[1].equals("UPDATE")){
 				cmd = 1;
 			}
@@ -88,34 +88,37 @@ public class Parser {
 			else if(teilStr[1].equals("ENDCON")){
 				cmd = 5;
 			}
+			else if(teilStr[1].equals("GETADMIN")) {
+				cmd = 6;
+			}
 		return cmd;
 	}
 	
 	public static String getNewInsert(String command) {
 		String teilStr[];
-		teilStr = command.split("/");
-		String eventStr= ","+ teilStr[2] + "," + teilStr[3]+  "," + teilStr[4] + "," + teilStr[5] + ",";
+		teilStr = command.split("//");
+		String eventStr= "::"+ teilStr[2] + "::" + teilStr[3]+  "::" + teilStr[4] + "::" + teilStr[5] + "::";
 		return eventStr;
 	}
 	
 	public static int getID(String command) {
 		String teilStr[];
-		teilStr = command.split("/");
+		teilStr = command.split("//");
 		String id = teilStr[2];
 		return Integer.parseInt(id);
 	}
 	
 	public static String getModifyStr(String command) {
 		String teilStr[];
-		teilStr = command.split("/");
-		String eventStr= ","+ teilStr[3] + "," + teilStr[4]+  "," + teilStr[5] + "," + teilStr[6] + ",";
+		teilStr = command.split("//");
+		String eventStr= "::"+ teilStr[3] + "::" + teilStr[4]+  "::" + teilStr[5] + "::" + teilStr[6] + "::";
 		System.out.println(eventStr);
 		return eventStr;
 	}
 	
 	public static boolean getPriv(String command) {
 		String teilStr[];
-		teilStr = command.split("/");
+		teilStr = command.split("//");
 		String priv = teilStr[4];
 		if(priv.equals("true")) {
 			return true;
