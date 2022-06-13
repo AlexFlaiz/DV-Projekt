d34ae82dbc97932af6351628bf47d200dd035c44
lukasrumpel <64522807+lukasrumpel@users.noreply.github.com>
2022-06-13 19:17:09 +0200

    Delete 2_Quellcode/TODO.net/ToDo-Liste/doc directory

--- 2_Quellcode/TODO.net/ToDo-Liste/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ToDo","l":"ClientParser"},{"p":"ToDo","l":"DateiHandler"},{"p":"ToDo","l":"Fenster"},{"p":"ToDo","l":"TODOs"}];updateSearchResults();
