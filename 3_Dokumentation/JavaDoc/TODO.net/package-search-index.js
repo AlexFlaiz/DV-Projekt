dab474db0dab8468c0c0614371d23467ad6b20ba
TobiasAberle <tobias.aberle@hs-furtwangen.de>
2022-06-24 09:58:01 +0200

    JavaDoc Installer und ToDo
    JavaDoc neu generiert, in Ordner 3 gespeichert

--- 3_Dokumentation/JavaDoc/TODO.net/package-search-index.js
+++ 3_Dokumentation/JavaDoc/TODO.net/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ToDo"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"Installer"},{"l":"ToDo"}];updateSearchResults();
